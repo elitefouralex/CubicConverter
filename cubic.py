3186ceef784f216032d885edc0996f192ebda0c9
Alex Oliveira <58787277+elitefouralex@users.noreply.github.com>
2021-01-07 01:06:57 -0800

    Update cubic.py
    Finally fixed code. works fully functioning now.

--- cubic.py
+++ cubic.py
@@ -1,36 +1,34 @@
 # ci = liters * 61.02
 # liters = ci / 61.02
 
-def youfuckedup():
-	print("""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-You done messed up A-A-RON!
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-""")
+def tryAgain():
+	print("\nPlease try again.")
 
 while True:
 	ci_or_liter = input("""
-Here we will be converting engine sizes to and from cubic inches and liters.
+Here we will be converting engine sizes to and from
+cubic inches and liters.
 	
-Which unit of measurement would you like to convert from? CI or L: """)
+Which unit of measurement would you like 
+to convert from? CI or L:\n> """)
 	
 	if ci_or_liter in ("CI", "ci", "Cubic Inches", "cubic inches", "cubic inch", "Cubic Inch"):
 		ci_amount = float(input("""
-How many cubic inches would you like to convert?: """))
+How many cubic inches would you like to convert?\n> """))
 		ci_output = ci_amount / 61.02
-		print(f"\n{ci_amount} equals {ci_output:.2f} liters.")
+		print(f"\n{ci_amount} cubic inches equals {ci_output:.2f} liters.")
 		break
-		if ci_amount is not 
-			continue
 	elif ci_or_liter in ("L", "l", "Liters", "Litres", "Liter","Litre", "liters", "litres", "liter", "litre"):
 		L_amount = float(input("""
-How many liters would you like to convert to cubic inches?: """))
+How many liters would you like to convert to cubic inches?\n> """))
 		L_output = L_amount * 61.02
 		#L_output with the colon dot 2 F rounds output of f string variable to 2 decimal places
-		print(f"\n{L_amount} is equal to {L_output:.2f}")
-		break	
+		print(f"\n{L_amount} liters is equal to {L_output:.2f} cubic inches.")
+		break
 	else:
-		youfuckedup()
+		tryAgain()
+		continue
+
 print("\nThanks for using my code!")
 
 
